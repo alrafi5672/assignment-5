8752092e4b1c5cfea18d77e2bb751ee95f77f5e3
alrafi5672 <alrafi5672@gmail.com>
2025-03-03 23:32:43 +0600

    final

--- js/completed.js
+++ js/completed.js
@@ -21,12 +21,39 @@ document.getElementById("yyyy").innerHTML = today.getFullYear();
 document.getElementById("box-btn").addEventListener("click",function(){
     window.location.href="./blog.html"
 })
-// completed-btn
-document.getElementById("com-btn-1").addEventListener("click",function(){
-    if (alert("gdeognh")){
-   
+// Add / delete
+document.addEventListener("DOMContentLoaded", function () {
+    const taskAssignedElement = document.querySelector(".task-assigned");
+    const completedTasksElement = document.querySelector(".completed-tasks");
+    const activityLog = document.querySelector(".activity-log");
+    const clearHistoryButton = document.querySelector(".clear-history");
 
+    document.querySelectorAll(".complete-button").forEach((button) => {
+        button.addEventListener("click", function () {
+            if (confirm("Board updated Successfully")) {
+                // Convert num
+                taskAssignedElement.textContent = +taskAssignedElement.textContent - 1;
+                completedTasksElement.textContent = +completedTasksElement.textContent + 1;
 
-    }    
-})
+                // Disable button
+                button.disabled = true;
+                button.textContent = "Completed";
+
+                // Get task
+                const taskHeader = button.closest(".task-card").querySelector(".task-header").textContent.trim();
+
+                // Add history
+                const logEntry = document.createElement("div");
+                logEntry.classList.add("bg-[#F4F7FF]")
+                logEntry.innerHTML = `<h1 class="font-medium pl-4 pt-3 pr-3 pb-1">You have Completed the ${taskHeader} at ${new Date().toLocaleTimeString()}</h1>`;
+                activityLog.appendChild(logEntry);
+            }
+        });
+    });
+    // clear history
+    clearHistoryButton.addEventListener("click", function () {
+        activityLog.innerHTML = "";
+    });
+});
+
 
